994a787ffed365a04145f80f94e4f2b8c48df6c3
Aryan <aryanshroff21@gmail.com>
2023-03-15 00:03:16 +0530

    Dual Theme Navigator

---
+++ src/Components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    Button,
+  } from '@chakra-ui/react';
+
+import {Link} from "react-router-dom";
+import { BiMenuAltLeft } from "react-icons/bi";
+
+const Header = () => {
+  return (
+    <>
+    
+    <Button>
+        <BiMenuAltLeft size={'20'}>
+
+        </BiMenuAltLeft>
+    </Button>
+    </>
+  )
+}
+
+export default Header
